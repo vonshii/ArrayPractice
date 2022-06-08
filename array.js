9f30798e3dbb68bb19f59f341f17493a85e3adec
vonshii <germanvscheidt@gmail.com>
2022-06-08 13:05:53 -0300

    Se agrego el archivo index y js con el primer ejercicio del practico

---
+++ array.js
@@ -0,0 +1,6 @@
+let meses = ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre'];
+
+
+for (let i = 0; i < meses.length; i++) {
+   document.write(meses[i] + '<br/>')
+}
