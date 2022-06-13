d59ec524ee709f40a63b0ebce4ce2d808f9ac081
German von Scheidt <germanvscheidt@gmail.com>
2022-06-13 13:54:57 -0300

    se se termino la funcion 3

--- function3.js
+++ function3.js
@@ -1,4 +1,4 @@
 let ladoA = parseInt(prompt("ingrese el valor del lado A del rectangulo"));
 let ladoB = parseInt(prompt("ingrese el valor del lado B del rectangulo"));
 
-document.write(`El perimetro del rectangulo es: ${(ladoA + ladoB)* 2}`)
+document.write(`El perimetro del rectangulo es: ${(ladoA + ladoB)* 2} cm`)
